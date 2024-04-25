9c6369ceb4163ddfa721dc5b33d586fc56853287
gyanendra-baghel <gyanendrabaghel633@gmail.com>
2024-04-25 11:40:53 -0700

    join student by emailId not userId

--- src/components/JoinStudent.jsx
+++ src/components/JoinStudent.jsx
@@ -1,38 +1,36 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
 
-function JoinStudent({classId}) {
-    const [studentId, setStudentId] = useState("");
-    const [loading, setLoading] = useState(false);
+function JoinStudent({ classId }) {
+  const [emailId, setEmailId] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      setLoading(true);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-      try {
-        const response = await axios.post(`/api/v1/classroom/${classId}/enroll`, {studentId});
-        if(response.data.statusCode == 200) {
-          console.log(response.data);
-          setStudentId("");
-        }
-      } catch (err) {
-        console.log(err);
+    try {
+      const response = await axios.post(`/api/v1/classroom/${classId}/enroll`, { emailId });
+      if (response.data.statusCode == 200) {
+        console.log(response.data);
+        setEmailId("");
       }
-
-      setLoading(false);
+    } catch (err) {
+      console.log(err);
     }
+
+    setLoading(false);
+  }
   return (
     <div className='mt-2'>
-        <div className='form'>
-                <form action="" onSubmit={handleSubmit}>
-                <h1>Add Student</h1>
-                <label htmlFor="">Student ID</label>
-                <input type="text" value={studentId} onChange={(e)=>{ setStudentId(e.target.value) }}/>
-                <button className='button'>
-                    {loading ? 'Creating...' : 'Join'}
-                </button>
-                </form>
-        </div>
+      <div className='form'>
+        <form action="" onSubmit={handleSubmit}>
+          <h1>Add Student</h1>
+          <label htmlFor="email">Email ID</label>
+          <input id='email' type="email" value={emailId} onChange={(e) => { setEmailId(e.target.value) }} />
+          <button className='button'>{loading ? 'Creating...' : 'Join'}</button>
+        </form>
+      </div>
     </div>
   )
 }
