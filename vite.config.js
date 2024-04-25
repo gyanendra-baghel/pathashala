11663bfe2b5bc69d0a45a4fd026495abad07333d
gyanendra-baghel <gyanendrabaghel633@gmail.com>
2024-04-25 11:51:44 -0700

    update proxy url with direct path

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({mode})=>{
   const env = loadEnv(mode, process.cwd());
 
-  const proxyApiUrl = env.VITE_API_URL || process.env.VITE_API_URL;
-
   return {
     plugins: [react()],
     server:{
       proxy:{
-        '/api': proxyApiUrl
+        '/api': "https://chat-backend-qj8w.onrender.com"
       }
     }
   }
