0f73fc5591d2ff2d85da86014dabc66fe8f4d029
gyanendra-baghel <gyanendrabaghel633@gmail.com>
2024-04-25 10:50:49 -0700

    setup env variables

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server:{
-    proxy:{
-      '/api':"http://localhost:8080"
+export default defineConfig(({mode})=>{
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server:{
+      proxy:{
+        '/api': env.VITE_API_URL
+      }
     }
   }
 })
